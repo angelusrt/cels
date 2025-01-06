61233a0c890a58b631a6c2bb0a46702c2c8b55ba
Angelus Rocha <angelusrt@gmail.com>
2025-01-05 22:25:26 -0300

    melhorando files

--- source/files.c
+++ source/files.c
@@ -1,32 +1,83 @@
 #include "files.h"
 
-estring files_read(file *f) {
-	long current_pos = ftell(f);
-	if (current_pos == -1) return (estring){.error=-1};
+estring files_read(file *self, const allocator *mem) {
+	long current_position = ftell(self);
+	if (current_position == -1) {
+		return (estring){.error=file_telling_position_error};
+	}
 
-	int seek_error = fseek(f, 0, SEEK_END);
-	if (seek_error == -1) return (estring){.error=-2};
+	int seek_error = fseek(self, 0, SEEK_END);
+	if (seek_error == -1) {
+		return (estring){.error=file_seeking_position_error};
+	}
 
-	long last_pos = ftell(f);
-	if (last_pos == -1) return (estring){.error=-1};
+	long last_position = ftell(self);
+	if (last_position == -1) {
+		return (estring){.error=file_telling_position_error};
+	}
 
-	seek_error = fseek(f, 0, SEEK_SET);
-	if (seek_error == -1) return (estring){.error=-2};
+	seek_error = fseek(self, 0, SEEK_SET);
+	if (seek_error == -1) {
+		return (estring){.error=file_seeking_position_error};
+	}
 
-	size_t new_capacity = maths_nearest_two_power(last_pos - current_pos);
-	string buffer = strings_init(new_capacity, null);
+	size_t new_capacity = last_position - current_position;
+	string buffer = strings_init(new_capacity, mem);
 
-	long bytes_read = fread(buffer.data, 1, buffer.capacity, f);
-	buffer.size = bytes_read + 1;
+	long bytes_read = fread(buffer.data, 1, buffer.capacity, self);
+	buffer.size = bytes_read;
 
-	bool did_file_not_end = feof(f) == 0;
-	int file_error = ferror(f);
-	long error = did_file_not_end+file_error;
+	int error = 0;
+	if (feof(self)) {
+		error = file_did_not_end_error;
+		goto error;
+	}
 
-	if (error != 0) {
-		strings_free(&buffer, null);
-		return (estring){.error=error};
+	if (ferror(self)) {
+		error = file_other_error;
+		goto error;
 	}
 
 	return (estring){.value=buffer};
+
+	error:
+	strings_free(&buffer, mem);
+	return (estring){.error=error};
+}
+
+estring_vec files_list(const string path, const allocator *mem) {
+	DIR *directory = opendir(path.data);
+	if (!directory) {
+		return (estring_vec){.error=file_directory_not_opened_error};
+	}
+
+	//
+
+	string_vec files = string_vecs_init(vector_min, mem);
+
+	//
+
+	struct dirent *entity;
+	while ((entity = readdir(directory))) {
+		if (entity->d_name[0] == '.') {
+			if (entity->d_name[1] == '\0') {
+				continue;
+			} else if (entity->d_name[1] == '.') {
+				if (entity->d_name[2] == '\0') {
+					continue;
+				}
+			}
+		}
+
+		string file = strings_make(entity->d_name, mem);
+
+		bool push_error = string_vecs_push(&files, file, mem);
+		if (push_error) {
+			strings_free(&file, mem);
+			string_vecs_free(&files, mem);
+			return (estring_vec){.error=file_allocation_error};
+		}
+	}
+
+	return (estring_vec){.value=files};
 }
--- source/files.h
+++ source/files.h
@@ -6,14 +6,34 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <dirent.h>
 
 #include "errors.h"
 #include "strings.h"
 #include "vectors.h"
+#include "mems.h"
 
 typedef FILE file;
 
+typedef enum file_error {
+	file_successfull,
+	file_telling_position_error,
+	file_seeking_position_error,
+	file_did_not_end_error,
+	file_other_error,
+	file_directory_not_opened_error,
+	file_allocation_error,
+} file_error;
+
+/*
+ */
 __attribute_warn_unused_result__
-estring files_read(file *f);
+estring files_read(file *f, const allocator *mem);
+
+/*
+ * #posix-only
+ */
+__attribute_warn_unused_result__
+estring_vec files_list(const string path, const allocator *mem);
 
 #endif
