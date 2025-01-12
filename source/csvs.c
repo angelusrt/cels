c6fddbb369b6e0f86ec7bf3d26022ce1082560d8
Angelus Rocha <angelusrt@gmail.com>
2025-01-12 17:52:19 -0300

    adicionando csvs

---
+++ source/csvs.c
@@ -0,0 +1,52 @@
+#include "csvs.h"
+
+string_bivec csvs_unmake(const string *text, const string column_sep, const allocator *mem) {
+	string_bivec csv = string_bivecs_init(vector_min, mem);
+	const string row_sep = strings_premake("\n");
+
+	string_vec row = strings_make_split(text, &row_sep, 0, mem);
+	for (size_t i = 0; i < row.size; i++) {
+		string_vec column = strings_make_split(&row.data[i], &column_sep, 0, mem);
+		string_bivecs_push(&csv, column, mem);
+	}
+
+	return csv;
+}
+
+bool csvs_split(string_vec *column_views, const string row_view, const string column_sep, const allocator *mem) {
+	string column_view = {0};
+	size_t k = 0;
+
+	while (!strings_next(&row_view, &column_sep, &column_view)) {
+		if (column_views->size <= k) {
+			bool upscale_error = string_vecs_upscale(column_views, mem);
+			if (upscale_error) {
+				return true;
+			}
+		}
+
+		column_views->data[k] = column_view;
+
+		k++;
+	}
+
+	column_views->size = k;
+	return column_view.data == null;
+}
+
+bool csvs_next(
+	const string *csv, 
+	string_vec *column_views, 
+	string *row_view, 
+	const string column_sep, 
+	const allocator *mem
+) {
+	const string row_sep = strings_premake("\n");
+	bool has_ended = strings_next(csv, &row_sep, row_view);
+	if (has_ended) {
+		return true;
+	}
+
+	return csvs_split(column_views, *row_view, column_sep, mem);
+}
+
---
+++ source/csvs.h
@@ -0,0 +1,55 @@
+#ifndef csvs_h
+#define csvs_h
+
+#include "strings.h"
+#include <sys/cdefs.h>
+
+/*
+ * Creates a matrix containing rows 
+ * and columns of csv text.
+ *
+ * A column-separator must be provided.
+ * Generally ',' or ';'.
+ *
+ * #allocates #to-review
+ */
+__attribute_warn_unused_result__
+string_bivec csvs_unmake(const string *text, const string column_sep, const allocator *mem);
+
+/*
+ * Splits csv-row into columns and puts 
+ * it into column_views -  a row_view must 
+ * be provided for this.
+ *
+ * A column-separator must be provided.
+ * Generally ',' or ';'.
+ *
+ * It upscales column_views automaticaly, 
+ * if it is uncapable of fitting columns.
+ *
+ * #allocates #to-review
+ */
+bool csvs_split(string_vec *column_views, const string row_view, const string column_sep, const allocator *mem);
+
+/*
+ * Iterates through csv rows, 
+ * updating row_view and puting 
+ * columns into column_views. 
+ *
+ * A column-separator must be provided.
+ * Generally ',' or ';'.
+ *
+ * It upscales column_views automaticaly, 
+ * if it is uncapable of fitting columns.
+ *
+ * #allocates #to-review
+ */
+bool csvs_next(
+	const string *csv, 
+	string_vec *column_views, 
+	string *row_view, 
+	const string column_sep, 
+	const allocator *mem
+);
+
+#endif
