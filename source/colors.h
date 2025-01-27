d0449594ffb6c8b8517122fb37da884f7b6c3e8e
Angelus Rocha <angelusrt@gmail.com>
2025-01-26 23:14:47 -0300

    iniciando colors

---
+++ source/colors.h
@@ -0,0 +1,14 @@
+#ifndef cels_colors_h
+#define cels_colors_h
+
+/* The module 'colors' implements, 
+ * well, colors.
+ */
+
+#define colors_error(text) "\033[31m" text "\033[0m\n"
+
+#define colors_warn(text) "\033[33m" text "\033[0m\n"
+
+#define colors_success(text) "\033[32m" text "\033[0m\n"
+
+#endif
