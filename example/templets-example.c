04a31c696d4a321765d5e433c0bc6baba88e416f
Angelus Rocha <angelusrt@gmail.com>
2025-02-09 22:56:05 -0300

    adicionando exemplo de templets

--- example/templets-example.c
+++ example/templets-example.c
@@ -1,7 +1,7 @@
 
-#include "../source/templates.h"
+#include "../source/templets.h"
 
-#include "../source/templates.c"
+#include "../source/templets.c"
 #include "../source/strings.c"
 #include "../source/vectors.c"
 #include "../source/nodes.c"
@@ -11,32 +11,33 @@
 #include "../source/files.c"
 
 static const string templ = strings_premake(
-	"<% define index %>\n"
+	"<| define index |>\n"
 	"<html>\n"
 	"<head>\n"
 	"</head>\n"
 	"<body>\n"
-	"<h1><% title %></h1>\n"
-	"<h2><% subtitle %></h2>\n"
-	"<p><% body %></p>\n"
+	"<h1><| title |></h1>\n"
+	"<h2><| subtitle |></h2>\n"
+	"<p><| body |></p>\n"
 	"</body>\n"
 	"</html>"
 );
 
-
 int main(void) {
 	const allocator mem = arenas_init(2049);
 
-	template_map templates = template_maps_init();
-	template_error parse_error = templates_parse(&templates, &templ, &mem);
+	templet_map templets = templet_maps_init();
+	templet_error parse_error = templets_parse(&templets, &templ, &mem);
 	if (parse_error != ok) {
 		printf("error '%d' happened while parsing.\n", parse_error);
-		goto cleanup;
+		goto cleanup0;
 	}
 
-	template_maps_println(&templates);
+	templet_maps_println(&templets);
 
-	cleanup:
+	cleanup0:
+	mem.debug(mem.storage);
+
 	mems_free(&mem, null);
 	return 0;
 }
